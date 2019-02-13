c0dcc743e1e28951fa3482e6d2ba7bf456febda6
Haramble <43665682+Haramble@users.noreply.github.com>
2019-02-13 21:58:35 +0900

    Update 15650 N과 M(2) dfs.cc

--- 15650 N과 M(2) dfs.cc
+++ 15650 N과 M(2) dfs.cc
@@ -1,9 +1,11 @@
 #include <iostream>
-
+/*
+간단한 dfs방법으로 nCm을 구한다.
+*/
 int N, M, arr[8];
 
 void sol(int cur, int arr_count){
-	if(arr_count == M){
+	if(arr_count == M){ //m개를 다 뽑았으면 1인것들만 출력
 		for(int i=0; i<N; i++){
 			if(arr[i] == 1){
 				printf("%d ",i+1);
@@ -13,10 +15,10 @@ void sol(int cur, int arr_count){
 		return;
 	}
 	
-	for(int i=cur; i<N; i++){
-		arr[i] = 1;
-		sol(i+1,arr_count+1);
-		arr[i] = 0;
+	for(int i=cur; i<N; i++){ //dfs반복
+		arr[i] = 1; //1로 했다가
+		sol(i+1,arr_count+1); //dfs하고
+		arr[i] = 0; //복구
 	}
 }
 
